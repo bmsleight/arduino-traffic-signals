8936f03e83f78cfff96ceb6afae6f06d21591359
bms@barwap.com <bms@barwap.com>
2014-04-16 12:25:07 +0100

    Bump

--- ats.ino
+++ ats.ino
@@ -1,35 +1,53 @@
 /*
-  ats - Arduino Traffic Signals
-  Turns an arduino in to a traffic signal controller.
- 
- (c) Brendan M. Sleight 2014
-
-#   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 2 of the License, or
-#   (at your option) any later version.
-#
-#   This program is distributed in the hope that it will be useful,
-#   but WITHOUT ANY WARRANTY; without even the implied warranty of
-#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#   GNU General Public License for more details.
-#
-#   You should have received a copy of the GNU General Public License
-#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
  */
+ 
+#include <Ats_phase.h>
 
-#include <phase.h>
+#define TOTAL_PHASES 2
 
-Phase a(TRAFFIC_PELICAN);
-Phase b(PED_PELICAN);
+Ats_phase phases[TOTAL_PHASES];
 
 
 void setup() {                
   // initialize the digital pin as an output.
+  // Pin 13 has an LED connected on most Arduino boards:
   pinMode(13, OUTPUT);     
+
+  phases[1].configure(TRAFFIC_JUNCTION, 14,15,16,0,0); // No Demand light (wait)
+  phases[2].configure(PED_JUNCTION, 7,0,8,9,10);  //No Amber
+  phases[2].setMinTimes(PHASE_GREEN,5);  
+  phases[2].setMinTimes(PHASE_BLACKOUT,4);  
+  phases[1].demand();
+  
 }
 
 void loop() {
+  digitalWrite(13, HIGH);   // set the LED on
   delay(1000);              // wait for a second
-}	
+  digitalWrite(13, LOW);    // set the LED off
+  delay(1000);              // wait for a second
+}
+
+void interupt_demands() {
+  for (unsigned char p = 0; p < TOTAL_PHASES; p++) {  
+    phases[p].detect();
+  }
+}
+  
+void interupt_tick()
+{
+
+
+}
+
+
+void decide_movements() {
+  // Want to fill this with better logic
+  // Move to a stage based signals
+  // At the moment only works for two phases
+  // One resting and the other run for min
+  // Assuming both conflict
+  // Want stages, conflict, intergreen later....
+  
+  
+}
